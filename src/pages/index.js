25e968108b5407ab64fb68d253d624ea84a4be8b
jcstein <mail@jcstein.dev>
2021-05-31 07:59:41 -0400

    string format date

--- src/pages/index.js
+++ src/pages/index.js
@@ -64,7 +64,7 @@ export const query = graphql`
           id
           frontmatter {
             title
-            date(formatString: "DD MMMM, YYYY")
+            date(formatString: "DD MMMM YYYY")
           }
           fields {
             slug
